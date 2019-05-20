9975e89a5d6e9b711034933943af37bb19cf9eb1
Samuel Walker <services@sfxworks.net>
2019-05-20 19:59:36 +0000

    Added var to pod spec

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => res.send('Hello World! Env Var KEY = ' + process.env.KEY))
+app.get('/', (req, res) => res.send('Hello World! The key for "ENV" == ' + process.env.KEY))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
